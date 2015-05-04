fa94888f0961265f04c2654268145b532f7d2751
Abhinav Manchanda <abhinav.manchanda@gmail.com>
2015-05-05 01:17:25 +0530

    Committing project skeleton.

---
+++ app/src/main/java/android/bootcamp/projectplanner/ProjectPlannerActivity.java
@@ -0,0 +1,17 @@
+package android.bootcamp.projectplanner;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class ProjectPlannerActivity extends ActionBarActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_project_planner);
+  }
+
+}
