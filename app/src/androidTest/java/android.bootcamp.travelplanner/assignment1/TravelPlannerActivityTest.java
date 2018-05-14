41aaf565e3d7012adb70a953daa892a669a32a2c
Sachin Sharma <sachin@dyulok.com>
2018-05-14 13:45:39 +0530

    <Skeleton> Modified test name, added test runner in gradle.

--- app/src/androidTest/java/android.bootcamp.travelplanner/assignment1/TravelPlannerActivityTest.java
+++ app/src/androidTest/java/android.bootcamp.travelplanner/assignment1/TravelPlannerActivityTest.java
@@ -3,9 +3,11 @@ package android.bootcamp.travelplanner.assignment1;
 
 import android.bootcamp.travelplanner.TravelPlannerActivity;
 import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
@@ -15,7 +17,7 @@ import static android.support.test.espresso.matcher.ViewMatchers.*;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
-// Tests for MainActivity
+@RunWith(AndroidJUnit4.class)
 public class TravelPlannerActivityTest {
 
     @Rule
