abhinavmanchanda/ProjectPlanner

Display time on a new Activity.

app/src/main/java/android/bootcamp/travelplanner/TimeActivity.java
@@ -1,0 +1,16 @@
+package android.bootcamp.travelplanner;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class TimeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_time);
+        TextView timeTextView = (TextView) findViewById(R.id.time);
+        timeTextView.setText(String.valueOf(getIntent().getIntExtra("time", -1)));
+    }
+}

chetankothari/TravelPlanner

app/src/main/java/android/bootcamp/travelplanner/TimeActivity.java
@@ -1,0 +1,16 @@
+package android.bootcamp.travelplanner;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class TimeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_time);
+        TextView timeTextView = (TextView) findViewById(R.id.time_new_activity);
+        timeTextView.setText(String.valueOf(getIntent().getIntExtra("time", -1)));
+    }
+}