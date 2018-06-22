0dd55d93603d5731bc1004f1d8a47f106e53caad
Chetan Kothari <mail@chetankothari.in>
2018-06-22 14:40:43 +0530

    Use data binding

--- app/src/main/java/android/bootcamp/travelplanner/TravelPlannerActivity.java
+++ app/src/main/java/android/bootcamp/travelplanner/TravelPlannerActivity.java
@@ -1,36 +1,35 @@
 package android.bootcamp.travelplanner;
 
 import android.app.Activity;
+import android.bootcamp.travelplanner.databinding.ActivityTravelPlannerBinding;
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
-
 
 public class TravelPlannerActivity extends Activity {
 
   public static final String BUFFERED_TIME = "android.bootcamp.travelplanner.BUFFERED_TIME";
   public static final int REQUEST_BUFFERED_TIME = 1;
 
+  private ActivityTravelPlannerBinding binding;
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_travel_planner);
+    binding = DataBindingUtil.setContentView(this, R.layout.activity_travel_planner);
   }
 
   public void calculate(View view) {
-    EditText distanceEdit = findViewById(R.id.distance);
-    String distanceString = distanceEdit.getText().toString();
+    String distanceString = binding.distance.getText().toString();
     int distance = Integer.parseInt(distanceString);
 
-    EditText velocityEdit = findViewById(R.id.velocity);
-    String velocityString = velocityEdit.getText().toString();
+    String velocityString = binding.velocity.getText().toString();
     int velocity = Integer.parseInt(velocityString);
 
     int time = distance / velocity;
 
-    TextView result = findViewById(R.id.time);
+    TextView result = binding.time;
     String strResult = String.valueOf(time);
     result.setText(strResult);
 
@@ -44,7 +43,7 @@ public class TravelPlannerActivity extends Activity {
     super.onActivityResult(requestCode, resultCode, data);
     if (requestCode == REQUEST_BUFFERED_TIME) {
       if (resultCode == RESULT_OK) {
-        TextView result = findViewById(R.id.buffered_time);
+        TextView result = binding.bufferedTime;
         result.setText(data.getStringExtra(BUFFERED_TIME));
       }
     }
